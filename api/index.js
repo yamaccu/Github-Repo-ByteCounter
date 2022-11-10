aa2c6cad2d3b953b3a4837568cfd6e09dbf30b99
yamaccu <yus1206@hotmail.com>
2022-11-10 16:58:49 +0900

    test

--- api/index.js
+++ api/index.js
@@ -28,7 +28,6 @@ export default async (req, res) => {
                 `,
         }
     })
-    .then(function(r){const ret = r})
+    .then(function(r){return res.send(r)})
 
-    return res.send(r.data);
 };
