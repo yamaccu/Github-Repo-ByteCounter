ad816c4b20b0b204e94015a65188eb34241993c0
yamaccu <yus1206@hotmail.com>
2022-11-10 18:10:51 +0900

    test

--- api/index.js
+++ api/index.js
@@ -4,31 +4,76 @@ export default async (req, res) => {
 
     return res.send(
         axios({
-            url: "https://api.github.com/graphql",
-            method: "post",
-            data: {
-                query: `
+                url: "https://api.github.com/graphql",
+                method: "post",
+                data: {
+                    query: `
                 query{
                     user(login:"yamaccu") {
-                    repositories(first:100 ,isFork:false){
-                        nodes{
-                        name
-                        languages(first:10) {
-                            edges {
-                            size
-                            node {
+                        repositories(first:100 ,isFork:false){
+                            nodes{
                                 name
-                            }
+                                languages(first:10) {
+                                    edges {
+                                        size
+                                        node {
+                                            name
+                                        }
+                                    }
+                                }
                             }
                         }
-                        }
-                    }
                     }
                 }
-                }
-                `,
+                `
+                },
                 Authorization: `bearer PAT_1`
-            }
         })
     );
 };
+
+
+/*
+axios({
+    url: "https://api.github.com/graphql",
+    method: "post",
+    headers,
+    data,
+  });
+
+  {
+    query: `
+    fragment RepoInfo on Repository {
+      name
+      nameWithOwner
+      isPrivate
+      isArchived
+      isTemplate
+      stargazers {
+        totalCount
+      }
+      description
+      primaryLanguage {
+        color
+        id
+        name
+      }
+      forkCount
+    }
+    query getRepo($login: String!, $repo: String!) {
+      user(login: $login) {
+        repository(name: $repo) {
+          ...RepoInfo
+        }
+      }
+      organization(login: $login) {
+        repository(name: $repo) {
+          ...RepoInfo
+        }
+      }
+    }
+  },
+  {
+    Authorization: `token ${token}`,
+  }
+  */
