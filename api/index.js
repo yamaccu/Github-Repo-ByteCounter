44001f1b9d5ed685e3533e12ef3f4fc79cb94e94
yamaccu <yus1206@hotmail.com>
2022-11-10 16:25:42 +0900

    test

--- api/index.js
+++ api/index.js
@@ -5,25 +5,28 @@ export default async (req, res) => {
     const ret = axios({
         url: "https://api.github.com/graphql",
         method: "post",
-        query:`
-    query{
-        user(login:"yamaccu") {
-          repositories(first:100 ,isFork:false){
-            nodes{
-              name
-              languages(first:10) {
-                edges {
-                  size
-                  node {
-                    name
-                  }
+        data: {
+            query: `
+                query{
+                    user(login:"yamaccu") {
+                    repositories(first:100 ,isFork:false){
+                        nodes{
+                        name
+                        languages(first:10) {
+                            edges {
+                            size
+                            node {
+                                name
+                            }
+                            }
+                        }
+                        }
+                    }
+                    }
                 }
-              }
-            }
-          }
+                }
+                `,
         }
-      }
-    `,
     });
 
     return res.send(ret);
