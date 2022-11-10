7b9d6463da28d856e9439fe3250672ec955f81ca
yamaccu <yus1206@hotmail.com>
2022-11-10 23:29:20 +0900

    test

--- api/index.js
+++ api/index.js
@@ -1,32 +1,79 @@
 import axios from "axios";
 
 export default async (req, res) => {
+  return res.send(retryer);
+};
 
-  const accessToken = 'PAT_1'
+const retryer = async (variables, retries = 0) => {
+  try {
+    // try to fetch with the first token since RETRIES is 0 index i'm adding +1
+    let response = await fetcher(
+      variables,
+      process.env[`PAT_1`],
+      retries,
+    );
 
-  return res.send(
+    // finally return the response
+    return response;
+  } catch (err) {
+    // prettier-ignore
+    // also checking for bad credentials if any tokens gets invalidated
+    const isBadCredential = err.response.data && err.response.data.message === "Bad credentials";
 
-    axios({
-      url: 'https://api.github.com/graphql',
-      headers: {
-        Authorization: `bearer ${accessToken}`,
-      },
-      method: 'POST',
-      data: {
-        query: `query { 
-             repository(owner: "vuejs", name: "vue") { 
-              name,
-              description,
-              license
+    if (isBadCredential) {
+      logger.log(`PAT_${retries + 1} Failed`);
+      retries++;
+      // directly return from the function
+      return retryer(fetcher, variables, retries);
+    } else {
+      return err.response;
+    }
+  }
+};
+
+const fetcher = (variables, token) => {
+  return request(
+    {
+      query: `
+      query userInfo($login: String!) {
+        user(login: $login) {
+          # fetch only owner repos & not forks
+          repositories(ownerAffiliations: OWNER, isFork: false, first: 100) {
+            nodes {
+              name
+              languages(first: 10, orderBy: {field: SIZE, direction: DESC}) {
+                edges {
+                  size
+                  node {
+                    color
+                    name
+                  }
+                }
+              }
             }
-          }`
+          }
+        }
       }
-    })
-
-
-
+      `,
+      variables,
+    },
+    {
+      Authorization: `token ${token}`,
+    },
   );
 };
+
+function request(data, headers) {
+  // @ts-ignore
+  return axios({
+    url: "https://api.github.com/graphql",
+    method: "post",
+    headers,
+    data,
+  });
+}
+
+
 
 
 /*
