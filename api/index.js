f7aff5d370388c71ffb8bac72c4bce60b60e68fe
yamaccu <yus1206@hotmail.com>
2022-11-23 00:32:22 +0900

    test

--- api/index.js
+++ api/index.js
@@ -138,7 +138,7 @@ async function fetchTopLanguages(repoNodes, exclude_repo = []) {
   return topLangs;
 }
 
-const requestGraphQL = async (variables, endCursor, previousData) => {
+const requestGraphQL = async (variables, endCursor, previousData = []) => {
   const token = process.env[`PAT_1`];
   const data =
   {
@@ -181,15 +181,14 @@ const requestGraphQL = async (variables, endCursor, previousData) => {
 
   const hasNextPage = resData.data.data.user.repositories.pageInfo.hasNextPage;
   endCursor = resData.data.data.user.repositories.pageInfo.endCursor;
+  resData = [...previousData, ...data.data.user.repositories.nodes]
 
   if(!hasNextPage)
   {
-    return resData.data.data.user.repositories.nodes;
+    return resData;
   }
 
-  resData = [previousData, ...data.data.user.repositories.nodes]
-
-  return requestGraphQL(variables, endCursor, resData.data);
+  return requestGraphQL(variables, endCursor, resData);
   //return resData;
 };
 
---
+++ api/test5.mjs
@@ -0,0 +1,115 @@
+import axios from "axios";
+
+const resGraphQL = await requestGraphQL({ login: "karaage0703" });
+console.log(resGraphQL);
+
+const topLangs = await fetchTopLanguages(resGraphQL);
+console.log(topLangs);
+
+async function requestGraphQL(variables, endCursor, previousData){
+  const token = `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`;
+  const data =
+  {
+    query: `
+      query userInfo($login: String!) {
+        user(login: $login) {
+          repositories(ownerAffiliations: OWNER, isFork: false, first: 100, ${endCursor ? `after: "${endCursor}"` : ''}) {
+            pageInfo{
+              hasNextPage
+              endCursor
+            }
+            nodes {
+              name
+              languages(first: 10, orderBy: {field: SIZE, direction: DESC}) {
+                edges {
+                  size
+                  node {
+                    name
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+      `,
+    variables,
+  };
+  const headers =
+  {
+    Authorization: `token ${token}`,
+  };
+
+  let resData = await axios({
+    url: "https://api.github.com/graphql",
+    method: "post",
+    headers,
+    data,
+  });
+
+  const hasNextPage = resData.data.data.user.repositories.pageInfo.hasNextPage;
+  endCursor = resData.data.data.user.repositories.pageInfo.endCursor;
+
+  if(!hasNextPage)
+  {
+    return resData.data.data.user.repositories.nodes;
+  }
+
+  resData = [previousData, ...resData.data.data.user.repositories.nodes]
+
+  return requestGraphQL(variables, endCursor, resData.data);
+  //return resData;
+};
+
+
+async function fetchTopLanguages(repoNodes, exclude_repo = []) {
+  let repoToHide = {};
+
+  console.log(repoNodes);
+
+  // populate repoToHide map for quick lookup
+  // while filtering out
+  if (exclude_repo) {
+    exclude_repo.forEach((repoName) => {
+      repoToHide[repoName] = true;
+    });
+  }
+
+  // filter out repositories to be hidden
+  repoNodes = repoNodes
+    .sort((a, b) => b.size - a.size)
+    .filter((name) => !repoToHide[name.name]);
+
+  repoNodes = repoNodes
+    .filter((node) => node.languages.edges.length > 0)
+    // flatten the list of language nodes
+    .reduce((acc, curr) => curr.languages.edges.concat(acc), [])
+    .reduce((acc, prev) => {
+      // get the size of the language (bytes)
+      let langSize = prev.size;
+
+      // if we already have the language in the accumulator
+      // & the current language name is same as previous name
+      // add the size to the language size.
+      if (acc[prev.node.name] && prev.node.name === acc[prev.node.name].name) {
+        langSize = prev.size + acc[prev.node.name].size;
+      }
+      return {
+        ...acc,
+        [prev.node.name]: {
+          name: prev.node.name,
+          color: prev.node.color,
+          size: langSize,
+        },
+      };
+    }, {});
+
+  const topLangs = Object.keys(repoNodes)
+    .sort((a, b) => repoNodes[b].size - repoNodes[a].size)
+    .reduce((result, key) => {
+      result[key] = repoNodes[key];
+      return result;
+    }, {});
+
+  return topLangs;
+}
