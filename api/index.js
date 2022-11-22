d8ea0d574c803bbaeadbb3000e8b09723d158d25
yamaccu <yus1206@hotmail.com>
2022-11-23 01:34:52 +0900

    test

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import * as dotenv from "dotenv";
+import { renderTopLanguages } from "../src/cards/top-languages-card.js";
+import {
+  parseArray,
+  parseBoolean,
+  renderError,
+} from "../src/common/utils.js";
+import {
+  logger,
+  MissingParamError,
+} from "../src/common/utils.js";
 import axios from "axios";
 
 dotenv.config();
@@ -6,38 +16,54 @@ dotenv.config();
 export default async (req, res) => {
   const {
     username,
-    exclude,
+    hide,
+    hide_title,
+    hide_border,
+    card_width,
+    title_color,
+    text_color,
+    bg_color,
+    theme,
+    layout,
+    langs_count,
+    exclude_repo,
+    custom_title,
+    locale,
+    border_radius,
+    border_color,
   } = req.query;
+  //res.setHeader("Content-Type", "image/svg+xml");
+  //res.setHeader("Content-Type", "application/json");
+  res.setHeader("Content-Type", "text/html");
 
   try {
-    if (!username) throw new Error (`username not found`);
-
-    const resGraphQL = await requestGraphQL({ login: username });
     const topLangs = await fetchTopLanguages(
-      resGraphQL,
-      parseArray(exclude),
+      username,
+      parseArray(exclude_repo),
     );
 
-    let rankColor = [];
-    let graphLength = [];
-    for(let i = 0; i < 4; i++)
-    {
-      rankColor[i] = calculateColor(topLangs[Object.keys(topLangs)[i]].size);
-      graphLength[i] = 100 / topLangs[Object.keys(topLangs)[0]].size * topLangs[Object.keys(topLangs)[i]].size;
-    }
-
-    res.setHeader("Content-Type", "image/svg+xml");
     res.setHeader("Cache-Control", `public, max-age=86400`);
 
     return res.send(`
+    <p>${Object.keys(topLangs)[0]}<p/>
+    <p>${Object.keys(topLangs)[1]}<p/>
+    <p>${Object.keys(topLangs)[2]}<p/>
+    <p>${Object.keys(topLangs)[3]}<p/>
+    <p>${Object.keys(topLangs)[4]}<p/>
+    `);
+
+
+    /*
+    return res.send(`
     <svg xmlns="http://www.w3.org/2000/svg" width="495" height="195" viewBox="0 0 495 195" fill="none" role="img"
     aria-labelledby="descId">
-    <title id="titleId">bytes Programmed</title>
-    <desc id="descId">bytes Programmed</desc>
+    <title id="titleId">yamaccu's GitHub Stats, Rank: A+</title>
+    <desc id="descId">Total Stars Earned: 2, Total Commits in 2022 : 2, Total PRs: 0, Total Issues: 2, Contributed to: 0
+    </desc>
     <style>
         .header {
             font: 600 18px 'Segoe UI', Ubuntu, Sans-Serif;
-            fill: #dc143c
+            fill: #2f80ed;
         }
         .lang-name {
             font: 600 12px 'Segoe UI', Ubuntu, Sans-Serif;
@@ -49,46 +75,58 @@ export default async (req, res) => {
         }
     </style>
     <rect x="0.5" y="0.5" rx="4.5" height="99%" stroke="#e4e2e2" width="494" fill="#fffefe" />
-    <g transform="translate(20, 30)">
-        <text class="header">Bytes Programmed</text>
-    </g>
     <svg x="25" width="445">
         <g transform="translate(0, 60)">
-            <text x="2" y="-5" class="lang-name">${Object.keys(topLangs)[0]}</text>
-            <text x="${Object.keys(topLangs)[0].length <= 10 ? 75 : 120}" y="-5" class="lang-name">${topLangs[Object.keys(topLangs)[0]].size.toLocaleString()} bytes</text>
-            <rect height="14" fill="${rankColor[0]}" x="0" y="0" data-testid="lang-progress" width="${graphLength[0]}%" />
+            <text x="2" y="-5" class="lang-name">1</text>
+            <rect height="14" fill="#e34c26" x="0" y="0" data-testid="lang-progress" width="100%" />
         </g>
         <g transform="translate(0, 95)">
-            <text x="2" y="-5" class="lang-name">${Object.keys(topLangs)[1]}</text>
-            <text x="${Object.keys(topLangs)[1].length <= 10 ? 75 : 120}" y="-5" class="lang-name">${topLangs[Object.keys(topLangs)[1]].size.toLocaleString()} bytes</text>
-            <rect height="14" fill="${rankColor[1]}" x="0" y="0" data-testid="lang-progress" width="${graphLength[1]}%" />
+            <text x="2" y="-5" class="lang-name">2</text>
+            <rect height="14" fill="#e34c26" x="0" y="0" data-testid="lang-progress" width="80%" />
         </g>
         <g transform="translate(0, 130)">
-            <text x="2" y="-5" class="lang-name">${Object.keys(topLangs)[2]}</text>
-            <text x="${Object.keys(topLangs)[2].length <= 10 ? 75 : 120}" y="-5" class="lang-name">${topLangs[Object.keys(topLangs)[2]].size.toLocaleString()} bytes</text>
-            <rect height="14" fill="${rankColor[2]}" x="0" y="0" data-testid="lang-progress" width="${graphLength[2]}%" />
+            <text x="2" y="-5" class="lang-name">3</text>
+            <rect height="14" fill="#e34c26" x="0" y="0" data-testid="lang-progress" width="40%" />
         </g>
         <g transform="translate(0, 165)">
-            <text x="2" y="-5" class="lang-name">${Object.keys(topLangs)[3]}</text>
-            <text x="${Object.keys(topLangs)[3].length <= 10 ? 75 : 120}" y="-5" class="lang-name">${topLangs[Object.keys(topLangs)[3]].size.toLocaleString()} bytes</text>
-            <rect height="14" fill="${rankColor[3]}" x="0" y="0" data-testid="lang-progress" width="${graphLength[3]}%" />
+            <text x="2" y="-5" class="lang-name">${topLangs.CSS.size}</text>
+            <rect height="14" fill="#e34c26" x="0" y="0" data-testid="lang-progress" width="5%" />
         </g>
     </svg>
 </svg>
      `
     );
+    /*
+      renderTopLanguages(topLangs, {
+        custom_title,
+        hide_title: parseBoolean(hide_title),
+        hide_border: parseBoolean(hide_border),
+        card_width: parseInt(card_width, 10),
+        hide: parseArray(hide),
+        title_color,
+        text_color,
+        bg_color,
+        theme,
+        layout,
+        langs_count,
+        border_radius,
+        border_color,
+        locale: locale ? locale.toLowerCase() : null,
+      }),
+    )
+    */
   } catch (err) {
     res.setHeader("Cache-Control", `no-cache, no-store, must-revalidate`); // Don't cache error responses.
-    res.setHeader("Content-Type", "text/html");
-    return res.send(
-    `
-    <p>${err}</p>
-    `
-    )
+    return res.send(renderError(err.message, err.secondaryMessage));
   }
 };
 
-async function fetchTopLanguages(repoNodes, exclude_repo = []) {
+async function fetchTopLanguages(username, exclude_repo = []) {
+  if (!username) throw new MissingParamError(["username"]);
+
+  const res = await requestGraphQL({ login: username });
+
+  let repoNodes = res.data.data.user.repositories.nodes;
   let repoToHide = {};
 
   // populate repoToHide map for quick lookup
@@ -138,24 +176,22 @@ async function fetchTopLanguages(repoNodes, exclude_repo = []) {
   return topLangs;
 }
 
-async function requestGraphQL(variables, endCursor, previousData = []){
+const requestGraphQL = async (variables) => {
   const token = process.env[`PAT_1`];
   const data =
   {
     query: `
       query userInfo($login: String!) {
         user(login: $login) {
-          repositories(ownerAffiliations: OWNER, isFork: false, first: 100, ${endCursor ? `after: "${endCursor}"` : ''}) {
-            pageInfo{
-              hasNextPage
-              endCursor
-            }
+          # fetch only owner repos & not forks
+          repositories(ownerAffiliations: OWNER, isFork: false, first: 100) {
             nodes {
               name
               languages(first: 10, orderBy: {field: SIZE, direction: DESC}) {
                 edges {
                   size
                   node {
+                    color
                     name
                   }
                 }
@@ -172,50 +208,10 @@ async function requestGraphQL(variables, endCursor, previousData = []){
     Authorization: `token ${token}`,
   };
 
-  let resData = await axios({
+  return axios({
     url: "https://api.github.com/graphql",
     method: "post",
     headers,
     data,
   });
-
-  const hasNextPage = resData.data.data.user.repositories.pageInfo.hasNextPage;
-  endCursor = resData.data.data.user.repositories.pageInfo.endCursor;
-  resData = [...previousData, ...resData.data.data.user.repositories.nodes]
-
-  if(!hasNextPage)
-  {
-    return resData;
-  }
-
-  return requestGraphQL(variables, endCursor, resData);
-  //return resData;
-};
-
-function calculateColor(size){
-  //#00ccff -> #0000ff / #ccff00 -> #00ff00 / #ffcc00 -> #ff0000
-  let r;
-  let g;
-  let b;
-  if(size < 100000)
-  {
-    g = 204 + Math.round(-204*size/100000);
-    return `#00` + g.toString(16).padStart(2,'0') + `ff`;
-  } 
-  if(size < 1000000)
-  {
-    r = 204 + Math.round(-204*size/1000000);
-    return `#` + r.toString(16).padStart(2,'0') + `ff00`;
-  } 
-  if(size < 10000000)
-  {
-    g = 204 + Math.round(-204*size/10000000);
-    return `#ff` + g.toString(16).padStart(2,'0') + `00`;
-  } 
-  return '#ff0000'
-};
-
-function parseArray(str) {
-  if (!str) return [];
-  return str.split(`,`);
 };
