928ee7fb18b932d82180d3b93befcc964b1276f4
yamaccu <yus1206@hotmail.com>
2022-11-22 18:45:29 +0900

    test

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ export default async (req, res) => {
 
     const resGraphQL = await requestGraphQL({ login: username });
 
-    throw new Error(resGraphQL.toString());
+    throw new Error(JSON.stringify(resGraphQL));
     
     const topLangs = await fetchTopLanguages(
       resGraphQL,
