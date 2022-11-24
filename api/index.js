9d1ec69fb390202d50c7c6c481ffcd06e845c036
yamaccu <yus1206@hotmail.com>
2022-11-24 23:49:11 +0900

    add test scripts

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 import * as dotenv from "dotenv";
-import axios from "axios";
+import requestGraphQL from "../src/requestGraphQL.js";
 
 dotenv.config();
 
@@ -134,64 +134,6 @@ function fetchTopLanguages(resGraphQL, exclude_repo = []) {
 
   return topLangs;
 }
-
-async function requestGraphQL(variables) {
-  let hasNextPage = true;
-  let endCursor;
-  let resData = [];
-  const token = process.env[`PAT_1`];
-  const headers =
-  {
-    Authorization: `token ${token}`,
-  };
-
-  let i = 0;
-  while (hasNextPage) {
-    let data =
-    {
-      query: `
-        query userInfo($login: String!) {
-          user(login: $login) {
-            repositories(ownerAffiliations: OWNER, isFork: false, first: 100, ${endCursor ? `after: "${endCursor}"` : ''}) {
-              pageInfo{
-                hasNextPage
-                endCursor
-              }
-              nodes {
-                name
-                languages(first: 10, orderBy: {field: SIZE, direction: DESC}) {
-                  edges {
-                    size
-                    node {
-                      color
-                      name
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-        `,
-      variables,
-    };
-  
-    let res = await axios({
-      url: "https://api.github.com/graphql",
-      method: "post",
-      headers,
-      data,
-    });
-
-    hasNextPage = res.data.data.user.repositories.pageInfo.hasNextPage;
-    endCursor = res.data.data.user.repositories.pageInfo.endCursor;
-
-    resData[i] = res.data;
-    i++;
-  }
-
-  return resData;
-};
 
 function calculateColor(size){
   //#00ccff -> #0000ff / #ccff00 -> #00ff00 / #ffcc00 -> #ff0000
---
+++ src/requestGraphQL.js
@@ -0,0 +1,62 @@
+import axios from "axios";
+
+export async function requestGraphQL(variables) {
+    let hasNextPage = true;
+    let endCursor;
+    let resData = [];
+    const token = `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`;
+    const headers =
+    {
+      Authorization: `token ${token}`,
+    };
+  
+    let i = 0;
+    while (hasNextPage) {
+      let data =
+      {
+        query: `
+          query userInfo($login: String!) {
+            user(login: $login) {
+              repositories(ownerAffiliations: OWNER, isFork: false, first: 100, ${endCursor ? `after: "${endCursor}"` : ''}) {
+                pageInfo{
+                  hasNextPage
+                  endCursor
+                }
+                nodes {
+                  name
+                  languages(first: 10, orderBy: {field: SIZE, direction: DESC}) {
+                    edges {
+                      size
+                      node {
+                        color
+                        name
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+          `,
+        variables,
+      };
+    
+      let res = await axios({
+        url: "https://api.github.com/graphql",
+        method: "post",
+        headers,
+        data,
+      });
+  
+      hasNextPage = res.data.data.user.repositories.pageInfo.hasNextPage;
+      endCursor = res.data.data.user.repositories.pageInfo.endCursor;
+  
+      resData[i] = res.data;
+      i++;
+    }
+  
+    return resData;
+  };
+
+
+export default requestGraphQL;
