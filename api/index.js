ee1437fc66a92619bee61ce9c154863dd9a181fb
yamaccu <yus1206@hotmail.com>
2022-11-25 00:12:00 +0900

    add test scripts

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import * as dotenv from "dotenv";
 import requestGraphQL from "../src/requestGraphQL.js";
+import fetchTopLanguages from "../src/fetchTopLanguages.js";
 
 dotenv.config();
 
@@ -87,53 +88,6 @@ export default async (req, res) => {
     )
   }
 };
-
-function fetchTopLanguages(resGraphQL, exclude_repo = []) {
-  let repoNodes = [];
-  for(let i = 0; i < resGraphQL.length; i++)
-  {
-    repoNodes = [...repoNodes, ...resGraphQL[i].data.user.repositories.nodes];
-  }  
-  let repoToHide = {};
-
-  if (exclude_repo) {
-    exclude_repo.forEach((repoName) => {
-      repoToHide[repoName] = true;
-    });
-  }
-
-  repoNodes = repoNodes
-    .sort((a, b) => b.size - a.size)
-    .filter((name) => !repoToHide[name.name]);
-
-  repoNodes = repoNodes
-    .filter((node) => node.languages.edges.length > 0)
-    .reduce((acc, curr) => curr.languages.edges.concat(acc), [])
-    .reduce((acc, prev) => {
-      let langSize = prev.size;
-
-      if (acc[prev.node.name] && prev.node.name === acc[prev.node.name].name) {
-        langSize = prev.size + acc[prev.node.name].size;
-      }
-      return {
-        ...acc,
-        [prev.node.name]: {
-          name: prev.node.name,
-          color: prev.node.color,
-          size: langSize,
-        },
-      };
-    }, {});
-
-  const topLangs = Object.keys(repoNodes)
-    .sort((a, b) => repoNodes[b].size - repoNodes[a].size)
-    .reduce((result, key) => {
-      result[key] = repoNodes[key];
-      return result;
-    }, {});
-
-  return topLangs;
-}
 
 function calculateColor(size){
   //#00ccff -> #0000ff / #ccff00 -> #00ff00 / #ffcc00 -> #ff0000
